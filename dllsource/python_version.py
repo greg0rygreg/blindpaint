6320238a9078fbfdb3fbbcc442ac2e133fcb90f5
greg <168974578+greg0rygreg@users.noreply.github.com>
2024-09-21 14:10:06 +0100

    update python version to be more verbose
    the C version and python version have the same amount of lines as of this commit

--- dllsource/python_version.py
+++ dllsource/python_version.py
@@ -1,8 +1,14 @@
-# first, import ctypes, comes preinstalled with python i think
-from ctypes import cdll
+# import ctypes, comes preinstalled with python i think
+import ctypes
 
 # reference the blindpaint lib by using LoadLibrary
-blindpaint = cdll.LoadLibrary("./blindpaint.so")
+blindpaint = ctypes.cdll.LoadLibrary("./blindpaint.so")
+
+# define arg and response types
+blindpaint.makeCanvas.argtypes = [ctypes.c_int, ctypes.c_int]; blindpaint.makeCanvas.restypes = ctypes.c_int
+blindpaint.paintPixel.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int]; blindpaint.makeCanvas.restypes = ctypes.c_int
+blindpaint.exportCanvas.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_wchar_p]; blindpaint.makeCanvas.restypes = ctypes.c_int
+blindpaint.freeCanvas.argtypes = [ctypes.c_int, ctypes.c_int]; blindpaint.makeCanvas.restypes = None
 
 # define rows and columns
 row, col = 3, 3
@@ -10,12 +16,15 @@ row, col = 3, 3
 # make the canvas
 canvas = blindpaint.makeCanvas(row, col)
 
-# paint some pixels
-blindpaint.paintPixel(canvas, 0, 0, 1)
-blindpaint.paintPixel(canvas, 0, 2, 1)
-blindpaint.paintPixel(canvas, 2, 0, 1)
-blindpaint.paintPixel(canvas, 2, 1, 1)
-blindpaint.paintPixel(canvas, 2, 2, 1)
+# if the canvas is None or NULL then exit
+if canvas == None: exit(1)
+
+# paint a few pixels
+blindpaint.paintPixel(canvas, 0,0, 1)
+blindpaint.paintPixel(canvas, 0,2, 1)
+blindpaint.paintPixel(canvas, 2,0, 1)
+blindpaint.paintPixel(canvas, 2,1, 1)
+blindpaint.paintPixel(canvas, 2,2, 1)
 # (these make a face just like in the C variant of this script)
 
 # save the canvas (for some weird reason you CAN'T use more than 1 char for the filename)
@@ -28,3 +37,4 @@ blindpaint.freeCanvas(canvas, col)
 # python3 python_version.py
 # or if you have python 3.11 installed like me:
 # python3.11 python_version.py
+# DO NOT FORGET TO PUT THE BLINDPAINT LIB IN THE SAME DIRECTORY AS OF THE MAIN SOURCE FILE!!!
